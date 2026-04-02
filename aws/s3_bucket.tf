197930ebf9b5cbbd89511ee97486fc4cc5c08f0f
Michael Santo Domingo <michaelsd.dev@gmail.com>
2026-04-02 21:03:45 +1100

    Add s3 bucket script

---
+++ aws/s3_bucket.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
+}
+
+resource "aws_bucket" "my_bucket" {
+  bucket = "my_bucket_${random_id.bucket_suffix.hex}"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
